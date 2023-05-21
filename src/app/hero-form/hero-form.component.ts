82d6aff63e0e4257953b480c02dde4501d8a3c71
Usuario <Usuario@DESKTOP-AVG34EV>
2023-05-20 23:21:07 -0500

    Agregar archivos de proyecto.

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { Hero1 } from '../hero1';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero1(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newHero() {
+    this.model = new Hero1(42, '', '');
+  }
+
+}
